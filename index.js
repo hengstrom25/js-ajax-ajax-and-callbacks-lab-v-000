8270b58f1f40b11609f1b7da5dbdfade0c1e9a87
Heidi Engstrom <hengstrom25@gmail.com>
2019-01-04 19:16:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ $(document).ready(function (){
 
 function searchRepositories() {
   const searchTerms = document.getElementsById('searchTerms').value
-  $.get('https://api.github.com/search/repositories')
+  $.get('https://api.github.com/search/repositories?q=${searchTerms}`, function(data)')
 }
