0d774c51a0d34f0c1334b2d42628c74fb216a275
Heidi Engstrom <hengstrom25@gmail.com>
2019-01-04 19:10:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 $(document).ready(function (){
 });
+
+function searchRepositories() {
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', showRepositories);
+  req.open('GET', 'https://api.github.com/users/octocat/repos');
+  req.send();
+}
