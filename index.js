af78eab7f4771645c9eff6fb87e94c6acc58d0c2
Heidi Engstrom <hengstrom25@gmail.com>
2019-01-04 19:20:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ $(document).ready(function (){
 });
 
 function searchRepositories() {
-  const searchTerms = document.getElementsById('searchTerms').value
+  const searchTerms = document.getElementById('searchTerms').value
   $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, function(data){$("#results").html(displayRepositories(data));
   }).fail(error =>
     displayError()
   )
+}
+
+function displayRepositories() {
+  return data.items.map(repo => renderRepositories(repo));
 }
 
 
